667bb5dfd31e0ccc90160cc277b88838c8c0e7c1
Martin <martinreuter24@googlemail.com>
2021-07-10 15:26:20 +0200

    test data for code2seq

--- approaches/code2seq/config.py
+++ approaches/code2seq/config.py
@@ -13,10 +13,10 @@ class Config:
         config.MAX_CONTEXTS = 200
         config.SUBTOKENS_VOCAB_MAX_SIZE = 190000
         config.TARGET_VOCAB_MAX_SIZE = 27000
-        config.EMBEDDINGS_SIZE = 128 * 2 #128
-        config.RNN_SIZE = 128 * 2  # Two LSTMs to embed paths, each of size 128
-        config.DECODER_SIZE = 320
-        config.NUM_DECODER_LAYERS = 1
+        config.EMBEDDINGS_SIZE = 128 * 4 #128 * 2 #128
+        config.RNN_SIZE = 128 * 4 #128 * 2  # Two LSTMs to embed paths, each of size 128
+        config.DECODER_SIZE = 512 #320
+        config.NUM_DECODER_LAYERS = 2 #1
         config.MAX_PATH_LENGTH = 8 + 1
         config.MAX_NAME_PARTS = 5
         config.MAX_TARGET_PARTS = 37 #6
