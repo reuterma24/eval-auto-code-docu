db101fc4f83a576b9a3535d391c9b9d3a9ab9673
Martin <martinreuter24@googlemail.com>
2021-07-16 16:51:46 +0200

    fixed formatting

--- datasets/funcom_filtered_reduced/preprocess_funcom.py
+++ datasets/funcom_filtered_reduced/preprocess_funcom.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import datasets.funcom_filtered_reduced.load as funcom
 import javalang
@@ -17,28 +18,31 @@ os.makedirs("./test", exist_ok=True)
 
 
 def __file_line(idx, body):
-    return "\t\"%s\": \"%s\",\n" % (idx, body)
+    return "\t\"%s\": \"%s\"" % (idx, body)
 
 
 def __refactor_codes():
     with open("test/functions.json", "w", encoding="utf-8") as funcs:
         funcs.write("{\n")
 
-        for i in codes_raw:
-            code = str(codes_raw[i])
+        for i, (k, v) in enumerate(codes_raw.items()):
+            code = str(v)
             codeParse = code.replace('\n', "").replace("\t", "")
-            codeWrite = code.replace('\n', "\\n").replace("\t", "\\t")
+            #codeWrite = code.replace('\n', "\\n").replace("\t", "\\t").replace('\"', '\\"')
+            codeWrite = code.encode('unicode_escape').decode('utf-8').replace('\"', '\\"')
             toParse = "class Parse {" + codeParse + '}'
 
             try:
                 javalang.parse.parse(toParse)
-                funcs.write(__file_line(i, codeWrite))
+                funcs.write(__file_line(k, codeWrite))
+                if i != (len(codes_raw) - 1):
+                    funcs.write(",\n")
             except Exception:
-                invalid_idx.append(str(i))
-                del comments_raw[i]
+                invalid_idx.append(str(k))
+                del comments_raw[k]
                 continue
 
-        funcs.write("}")
+        funcs.write("\n}")
         funcs.close()
 
 
@@ -46,18 +50,20 @@ def __refactor_comments():
     with open("test/comments.json", "w", encoding="utf-8") as coms:
         coms.write("{\n")
 
-        for i in comments_raw:
-            comment = str(comments_raw[i])
+        for i, (k, v) in enumerate(comments_raw.items()):
+            comment = str(v)
 
             # if no '.' I assume its a single sentence
             if '.' in comment:
                 split = comment.split(sep='.')
                 comment = split[0] + "*/\n"
 
-            comment = comment.replace('\n', "\\n").replace("\t", "\\t")
-            coms.writelines(__file_line(i, comment))
+            comment = comment.encode('unicode_escape').decode('utf-8').replace('\"', '\\"')
+            coms.writelines(__file_line(k, comment))
+            if i != (len(comments_raw) - 1):
+                coms.write(",\n")
 
-        coms.write("}")
+        coms.write("\n}")
         coms.close()
 
 
