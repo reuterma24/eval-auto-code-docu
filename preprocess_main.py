1a5ec1e183c9ad88f5508229be0584baa6b2848e
Martin <martinreuter24@googlemail.com>
2021-08-30 16:12:51 +0200

    use full dataset

--- preprocess_main.py
+++ preprocess_main.py
@@ -7,18 +7,9 @@ from datasets.funcom_filtered_reduced import javaparse_funcom
 
 root = os.path.dirname(os.path.abspath(__file__))
 
-N = 100  # number of code comment pairs
-train_size = 0.8
-test_size = 0.1
-validation_size = 0.1
-
-msg = "%s Code Comment Pairs used in general. %s for training, %s for testing and %s for validating." \
-     % (str(N), str(train_size * N), str(test_size * N), str(validation_size * N))
-print(msg)
-
-reduce_script = root + ("/datasets/reduce_funcom_filtered.sh %s" % (str(N)))
-process = Popen(reduce_script, shell=True)
-process.wait()
+#reduce_script = root + ("/datasets/reduce_funcom_filtered.sh %s" % (str(N)))
+#process = Popen(reduce_script, shell=True)
+#process.wait()
 
 # REMOVE INVALID SYNTAX AND ONLY TAKE FIRST SENTENCE OF COMMENT
 #print("Startin to preprocess funcom - finding invalid function/comment ids")
