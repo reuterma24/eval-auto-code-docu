f4f09b39cb723961d3970517523697143316aabf
Martin <martinreuter24@googlemail.com>
2021-09-07 22:08:01 +0200

    code2seq test

--- approaches/code2seq/single-preprocess.sh
+++ approaches/code2seq/single-preprocess.sh
@@ -1,11 +1,12 @@
 TRAIN_DIR=preprocessing/code2seq/preprocessed_data/split/training
+TRAIN_DATA_FILE=funcom.train.raw.txt
 
 echo "Extracting paths from specific training files "
 for number in 5
 #8 14 15 47 62 72 73 90 98 99 100 120 128 132
 do
   echo "train file ${number}"
-  ${PYTHON} approaches/code2seq/JavaExtractor/extract.py --file "${TRAIN_DIR}/train${number}.java" --max_path_length 8 --max_path_width 2 --num_threads ${NUM_THREADS} --jar ${EXTRACTOR_JAR} | shuf > ${COUNTER}${TRAIN_DATA_FILE} 2>> error_log.txt
-  ((COUNTER = COUNTER+1))
+  ${PYTHON} approaches/code2seq/JavaExtractor/extract.py --file "${TRAIN_DIR}/train${number}.java" --max_path_length 8 --max_path_width 2 --num_threads ${NUM_THREADS} --jar ${EXTRACTOR_JAR} | shuf > ${number}${TRAIN_DATA_FILE} 2>> error_log.txt
+
 done
 echo "Finished extracting paths"
