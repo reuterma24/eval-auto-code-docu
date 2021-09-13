fd1561fdea72654fd3a5f17af48e19737bce5807
Martin <martinreuter24@googlemail.com>
2021-09-13 10:21:31 +0200

    adjusted to funcom

--- preprocessing/preprocess_funcom_gnn.py
+++ preprocessing/preprocess_funcom_gnn.py
@@ -1,4 +1,16 @@
 import pickle
+import argparse
+import os
+import random
+import sys
+import time
+import traceback
+import numpy as np
+import tensorflow as tf
+from keras.callbacks import ModelCheckpoint, Callback
+import keras.backend as K
+from approaches.gnn.utils.model import create_model
+from approaches.gnn.utils.myutils import batch_gen, init_tf
 
 print("Removing invalid fids from dataset.pkl")
 
