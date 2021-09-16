8b7ce6083e56679e468c301d85cb0afbb67a2c13
Martin <martinreuter24@googlemail.com>
2021-09-16 17:11:55 +0200

    adjusted prediction

--- evaluation/create_reference_list.py
+++ evaluation/create_reference_list.py
@@ -8,6 +8,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
     prediction_file = args.path
 
+
+
     f = open(prediction_file, 'rb')
     preds = f.readlines()
     print("examples: " + str(len(preds)))
@@ -15,14 +17,24 @@ if __name__ == '__main__':
     fid = list()
     prediciton = list()
 
-    for pred in preds:
-        a, b = str(pred).split("<s>")
-        c = b.split("</s>")
-        print(a)
-        print(str(a))
-        fid.append(int(a))
-        prediciton.append(str(c[0]))
+  #  for pred in preds:
+   #     a, b = str(pred).split("<s>")
+    #    c = b.split("</s>")
+    #    print(a.strip())
+    #    print(str(a))
+    #    fid.append(int(a))
+    #    prediciton.append(str(c[0]))
+
+    preds = dict()
+    predicts = open(prediction_file, 'r')
+    for c, line in enumerate(predicts):
+        (fid, pred) = line.split('\t')
+        fid = int(fid)
+        pred = pred.split()
+        preds[fid] = pred
+    predicts.close()
+
 
     with open("test.txt", 'w') as f:
-        for i in enumerate(fid):
-            f.writelines(fid[i] + ": " + prediciton[i])
+        for k, v in predicts.items():
+            f.writelines(k + ": " + v)
