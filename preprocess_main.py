f9ed53f7c16a24e7766cb1651bab679dcb60139b
Martin <martinreuter24@googlemail.com>
2021-09-17 01:09:18 +0200

    adjusted prediction

--- preprocess_main.py
+++ preprocess_main.py
@@ -4,7 +4,7 @@ import preprocessing.preprocess_general as general
 from subprocess import Popen
 
 
-#root = os.path.dirname(os.path.abspath(__file__))
+root = os.path.dirname(os.path.abspath(__file__))
 
 reduce_script = root + ("/datasets/reduce_funcom_filtered.sh %s" % (str(500)))
 process = Popen(reduce_script, shell=True)
