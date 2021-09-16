d31304da925c281f54ad7b69e3f8ab864ded03f0
Martin Reuter <reuterma@gruenau2>
2021-09-17 00:20:39 +0200

    adjustment

--- evaluation/history_checker.py
+++ evaluation/history_checker.py
@@ -1,11 +1,18 @@
 import os;
 import pickle;
+import argparse
 
-with open("attendgru_hist_1631539477.pkl", "rb") as hist:
-	data = pickle.load(hist)
-	print(data.keys())
-	val_scores = data['val_accuracy']
-	for i, v in enumerate(val_scores):
-		number = str(v)
-		print("epoch " + str(i+1)+ ': ' + number[0:6])
+if __name__ == '__main__':
+        parser = argparse.ArgumentParser(description='')
+        parser.add_argument('path' ,type=str, default=None)
+        args=parser.parse_args()
+        path = args.path
+
+        with open(path, "rb") as hist:
+                data = pickle.load(hist)
+                print(data.keys())
+                val_scores = data['val_accuracy']
+                for i, v in enumerate(val_scores):
+                        number = str(v)
+                        print("epoch " + str(i+1)+ ': ' + number[0:6])
 
