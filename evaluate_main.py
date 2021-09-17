f1887621e7c2deb40d0986c7c4755b49e1e352d4
Martin <martinreuter24@googlemail.com>
2021-09-17 03:17:24 +0200

    adjusted prediction

--- evaluate_main.py
+++ evaluate_main.py
@@ -51,6 +51,10 @@ for key in sorted(refs_dict.keys()):
     refs.append(refs_dict[key])
     preds.append(preds_dict[key])
 
+
+for s in preds:
+    if not s:
+        print("FUCK")
 print("refs :" + str((len(refs))))
 print("preds :" + str((len(preds))))
 print("done")
