b099a13e067c1c8e52307ea9d06145ad87a50b9d
Martin <martinreuter24@googlemail.com>
2021-09-28 19:26:19 +0200

    commit

--- preprocess_main.py
+++ preprocess_main.py
@@ -5,9 +5,8 @@ import preprocessing.preprocess_pkl_caller as caller
 import preprocessing.preprocess_pkl_caller2 as caller2
 from subprocess import Popen
 
-
+### HELPFUL SCRIPT TO PRODUCE A SUBSET OF FUNCOM FOR WORKING ON A LOCAL MACHINE ###
 #root = os.path.dirname(os.path.abspath(__file__))
-
 #reduce_script = root + ("/datasets/reduce_funcom_filtered.sh %s" % (str(3000)))
 #process = Popen(reduce_script, shell=True)
 #process.wait()
@@ -15,16 +14,16 @@ from subprocess import Popen
 
 # GENERAL PREPROCESSING
 print("Applying general preprocessing")
-#general.preprocess()
+general.preprocess()
 
 # RUN CODE2SEQ PREPROCESSING
 print("Preprocessing data for code2seq")
-#code2seq.preprocess()
+code2seq.preprocess()
 
 
 ### THE FOLLOWING 'CALLERS' ARE NEEDED BECAUSE OF OCCURING PICKLE ERRORS
 # RUN .PKL PREPROCESSING FOR GNN, ATTNFC
-#caller.preprocess()
+caller.preprocess()
 
 # RUN .PKL PREPROCESSING FOR NEURALLSPS
 caller2.preprocess()
